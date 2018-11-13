a9ed26704d10fcb9b5bdedf2298a90c0fe0180cf
Administrator <Administrator@DESKTOP-099UQRG>
2018-11-12 21:15:32 -0500

    Resize photo with thumbnailator

--- plantplacespostprocessor/src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
+++ plantplacespostprocessor/src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
@@ -1,7 +1,14 @@
 package com.plantplaces.photos.plantplacespostprocessor;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 @Component
 public class PhotoProcessor {
@@ -9,7 +16,18 @@ public class PhotoProcessor {
 	@JmsListener(destination="photos")
 	public void processPhoto(String path) {
 		System.out.println("Path is: " + path);
-		int i = 1 + 1;
+		File file = new File(path);
+		Path thumbNailPath = Paths.get(path).getParent();
+		Path fileName = Paths.get(path).getFileName();
+		String thumbnailFullPath = thumbNailPath + File.separator + "thumbnail" + File.separator + fileName;
+		File thumbnailFile = new File(thumbnailFullPath);
+		try {
+			Thumbnails.of(file).size(100, 100).toFile(thumbnailFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
