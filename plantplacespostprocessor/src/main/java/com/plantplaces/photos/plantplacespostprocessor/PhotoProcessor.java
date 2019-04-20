708961b62782cdcb083314574fc0ebed4e070922
Administrator <Administrator@DESKTOP-099UQRG>
2019-04-19 20:44:46 -0400

    Replace queue reader with Kafka topic reader.

--- plantplacespostprocessor/src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
+++ plantplacespostprocessor/src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import javax.imageio.ImageIO;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import net.coobird.thumbnailator.Thumbnails;
@@ -17,18 +18,18 @@ import net.coobird.thumbnailator.geometry.Positions;
 @Component
 public class PhotoProcessor {
 
-	@JmsListener(destination="photos")
+	@KafkaListener(topics="test", groupId="plantplaces")
 	public void processPhoto(String path) {
-		System.out.println("Path is: " + path);
-		File file = new File(path);
-		Path photoPath = Paths.get(path).getParent();
-		Path fileName = Paths.get(path).getFileName();
-		String thumbnailFullPath = photoPath + File.separator + "thumbnail" + File.separator + fileName;
-		File thumbnailFile = new File(thumbnailFullPath);
-		String watermarkPath = photoPath + File.separator + "watermark.png";
-		File watermarkFile = new File(watermarkPath);
-		
 		try {
+			System.out.println("Path is: " + path);
+			File file = new File(path);
+			Path photoPath = Paths.get(path).getParent();
+			Path fileName = Paths.get(path).getFileName();
+			String thumbnailFullPath = photoPath + File.separator + "thumbnail" + File.separator + fileName;
+			File thumbnailFile = new File(thumbnailFullPath);
+			String watermarkPath = photoPath + File.separator + "watermark.png";
+			File watermarkFile = new File(watermarkPath);
+		
 			BufferedImage watermark = ImageIO.read(watermarkFile);
 			Thumbnails.of(file).scale(1).watermark(Positions.BOTTOM_RIGHT, watermark, 0.9f).toFile(file);
 			Thumbnails.of(file).size(100, 100).toFile(thumbnailFile);
